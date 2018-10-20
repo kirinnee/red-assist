06905bc6a8deca98a8db596dd73dc89a97b40cb2
Ernest <ernest.innosparks@gmail.com>
2018-10-21 00:27:43 +0800

    babel-ed

--- injection.js
+++ injection.js
@@ -1,9 +1,8 @@
-setInterval(function(){
-   let toolbar = document.getElementById("toolbar");
-   if(toolbar != null)
-   toolbar.style.display = "none";
-   let watermark = document.querySelectorAll("#resizer > div:nth-child(16)");
-   if(watermark!=null && watermark[0] != null)
-    watermark[0].style.display = "none";
+"use strict";
 
-},1);
+setInterval(function () {
+   var toolbar = document.getElementById("toolbar");
+   if (toolbar != null) toolbar.style.display = "none";
+   var watermark = document.querySelectorAll("#resizer > div:nth-child(16)");
+   if (watermark != null && watermark[0] != null) watermark[0].style.display = "none";
+}, 1);
